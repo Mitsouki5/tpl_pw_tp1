66d14277b2073b3c8604e96d0fc1c953f72a5a33
Antoine <antdaoust@gmail.com>
2024-09-07 18:44:34 -0400

    Ajout php pour page des recettes

--- nosrecettes/tpl-recettes.php
+++ nosrecettes/tpl-recettes.php
@@ -1,87 +1,148 @@
+<?php /* Template Name: Toutes les recettes */ ?>
 <?php get_header(); ?>
 
 <section class="heroRecettes">
     <div class="wrapper">
-        <h1>Toutes Nos Recettes</h1>
+        <h1><?php the_title(); ?></h1>
     </div>
 </section>
 
 <section class="recettes">
     <div class="wrapper">
-        <h2>Viandes</h2>
-        <div class="cards">
-            <article class="card">
-                <a href="single-recette.html">
-                    <div class="card_media"><img src="assets/images/spaghetti.png" alt="spaghetti" /></div>
-                    <div class="card_content">
-                        <h3>Titre de la recette</h3>
-                        <p>Courte description d'une recette</p>
-                        <h4>Catégorie</h4>
-                    </div>
-                </a>
-            </article>
+    <?php 
+                //Affiche 2 jeux du plus ancien au plus récent (sur la base d'un champs ACF)
+                $arg = array(
+                    'post_type' => 'recette',
+                    'post_status' => 'publish',
+                    'posts_per_page' => '2',
+                    'order' => 'rand',
+                    'category_name' => 'viande',
+                );
 
-            <article class="card">
-                <a href="single-recette.html">
-                    <div class="card_media"><img src="assets/images/spaghetti.png" alt="spaghetti" /></div>
-                    <div class="card_content">
-                        <h3>Titre de la recette</h3>
-                        <p>Courte description d'une recette</p>
-                        <h4>Catégorie</h4>
-                    </div>
-                </a>
-            </article>
-        </div>
+                $query = new WP_Query($arg);
+        ?>
 
-        <h2>Végétariens</h2>
-        <div class="cards">
-            <article class="card">
-                <a href="single-recette.html">
-                    <div class="card_media"><img src="assets/images/spaghetti.png" alt="spaghetti" /></div>
-                    <div class="card_content">
-                        <h3>Titre de la recette</h3>
-                        <p>Courte description d'une recette</p>
-                        <h4>Catégorie</h4>
-                    </div>
-                </a>
-            </article>
+        <?php if ($query->have_posts()) : ?>
+            <h2>Viandes</h2>
+            <div class="cards">
+                <?php while ($query->have_posts()) : $query->the_post(); ?>     
+                    <article class="card">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="card_media">
+                                <?php $image = get_field('thumbnail');
+                                    if( !empty( $image ) ): ?>
+                                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                                    <?php endif; ?>
+                            </div>
+                            <div class="card_content">
+                                <h3><?php the_title(); ?></h3>
+                                <p><?php the_excerpt(); ?></p>
 
-            <article class="card">
-                <a href="single-recette.html">
-                    <div class="card_media"><img src="assets/images/spaghetti.png" alt="spaghetti" /></div>
-                    <div class="card_content">
-                        <h3>Titre de la recette</h3>
-                        <p>Courte description d'une recette</p>
-                        <h4>Catégorie</h4>
-                    </div>
-                </a>
-            </article>
-        </div>
+                                <?php $categories = array(); ?>
+                                <?php foreach(get_the_category() as $category) : ?>
+                                    <?php array_push($categories, $category->name); ?>
+                                <?php endforeach; ?>  
+                                <?php if ($categories) : ?>
+                                    <h4><?php echo implode(', ', $categories); ?></h4>
+                                <?php endif; ?>
 
-        <h2>Desserts</h2>
-        <div class="cards">
-            <article class="card">
-                <a href="single-recette.html">
-                    <div class="card_media"><img src="assets/images/spaghetti.png" alt="spaghetti" /></div>
-                    <div class="card_content">
-                        <h3>Titre de la recette</h3>
-                        <p>Courte description d'une recette</p>
-                        <h4>Catégorie</h4>
-                    </div>
-                </a>
-            </article>
+                            </div>
+                        </a>
+                    </article>
+                <?php endwhile ?>
+            </div>
+        <?php endif ?>
+        <?php wp_reset_postdata(); ?>
 
-            <article class="card">
-                <a href="single-recette.html">
-                    <div class="card_media"><img src="assets/images/spaghetti.png" alt="spaghetti" /></div>
-                    <div class="card_content">
-                        <h3>Titre de la recette</h3>
-                        <p>Courte description d'une recette</p>
-                        <h4>Catégorie</h4>
-                    </div>
-                </a>
-            </article>
-        </div>
+        <?php 
+                //Affiche 2 jeux du plus ancien au plus récent (sur la base d'un champs ACF)
+                $arg = array(
+                    'post_type' => 'recette',
+                    'post_status' => 'publish',
+                    'posts_per_page' => '2',
+                    'order' => 'rand',
+                    'category_name' => 'salade',
+                );
+
+                $query = new WP_Query($arg);
+        ?>
+
+        <?php if ($query->have_posts()) : ?>
+            <h2>Végétariens</h2>
+            <div class="cards">
+                <?php while ($query->have_posts()) : $query->the_post(); ?>     
+                    <article class="card">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="card_media">
+                                <?php $image = get_field('thumbnail');
+                                    if( !empty( $image ) ): ?>
+                                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                                    <?php endif; ?>
+                            </div>
+                            <div class="card_content">
+                                <h3><?php the_title(); ?></h3>
+                                <p><?php the_excerpt(); ?></p>
+
+                                <?php $categories = array(); ?>
+                                <?php foreach(get_the_category() as $category) : ?>
+                                    <?php array_push($categories, $category->name); ?>
+                                <?php endforeach; ?>  
+                                <?php if ($categories) : ?>
+                                    <h4><?php echo implode(', ', $categories); ?></h4>
+                                <?php endif; ?>
+
+                            </div>
+                        </a>
+                    </article>
+                <?php endwhile ?>
+            </div>
+        <?php endif ?>
+        <?php wp_reset_postdata(); ?>
+
+        <?php 
+                //Affiche 2 jeux du plus ancien au plus récent (sur la base d'un champs ACF)
+                $arg = array(
+                    'post_type' => 'recette',
+                    'post_status' => 'publish',
+                    'posts_per_page' => '2',
+                    'order' => 'rand',
+                    'category_name' => 'dessert',
+                );
+
+                $query = new WP_Query($arg);
+        ?>
+
+        <?php if ($query->have_posts()) : ?>
+            <h2>Desserts</h2>
+            <div class="cards">
+                <?php while ($query->have_posts()) : $query->the_post(); ?>     
+                    <article class="card">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="card_media">
+                                <?php $image = get_field('thumbnail');
+                                    if( !empty( $image ) ): ?>
+                                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                                    <?php endif; ?>
+                            </div>
+                            <div class="card_content">
+                                <h3><?php the_title(); ?></h3>
+                                <p><?php the_excerpt(); ?></p>
+
+                                <?php $categories = array(); ?>
+                                <?php foreach(get_the_category() as $category) : ?>
+                                    <?php array_push($categories, $category->name); ?>
+                                <?php endforeach; ?>  
+                                <?php if ($categories) : ?>
+                                    <h4><?php echo implode(', ', $categories); ?></h4>
+                                <?php endif; ?>
+
+                            </div>
+                        </a>
+                    </article>
+                <?php endwhile ?>
+            </div>
+        <?php endif ?>
+        <?php wp_reset_postdata(); ?>
     </div>
 </section>
 
